ebf16986b24a02b3cdf7267e6cb2ca26baa36589
Irene Fuente Romero <ifuenteromer@gmail.com>
2023-06-09 12:35:10 +0200

    Exercise

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,12 @@
 let mySize;
 mySize = 5;
 console.log(mySize);
+const calculate = (income) => {
+    const test = 500000;
+    console.log({ test });
+    if (income > test)
+        return 3;
+    return 2;
+};
+calculate(2);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,51 @@ mySize = Size.Large
 console.log(mySize);
 
 
+const calculate = (income: number): number => {
+    const test = 5_000_00;
+    console.log({ test });
+
+    if (income > test) return 3
+    return 2
+
+}
+
+calculate(2);
+
+let a = 100; let b = 'Coffee'; let c = [true, false, false];
+let d = { age: 20 };
+let e = [3]; let f; let g = []
+
+const song: {
+    title: string,
+    releaseYear?: number
+} = { title: 'My song' }
+
+type Age = number
+
+type User = {
+    name: string,
+    age: Age,
+    occupation?: string
+}
+
+const users: User[] = [{ name: 'John Smith', age: 30, occupation: 'Software engineer' }, { name: 'Kate Müller', age: 28 }]
+
+type Fish = {
+    swim: () => void
+}
+
+type Bird = {
+    fly: () => void
+}
+
+type Pet = Fish | Bird;
+
+const pet: Pet = {
+    swim: () => console.log('swim'),
+}
+
+type DayOfWeek = 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Monday'
+const day: DayOfWeek = 'Monday'
+
+
