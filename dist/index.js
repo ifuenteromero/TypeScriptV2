dff24362cd274af38c00ba9f15175fd60fd54a73
Irene Fuente Romero <ifuenteromero@gmail.com>
2023-06-14 18:38:41 +0200

    Creating classes

--- dist/index.js
+++ dist/index.js
@@ -10,4 +10,17 @@ const calculate = (income) => {
     return 2;
 };
 calculate(2);
+let a = 100;
+let b = 'Coffee';
+let c = [true, false, false];
+let d = { age: 20 };
+let e = [3];
+let f;
+let g = [];
+const song = { title: 'My song' };
+const users = [{ name: 'John Smith', age: 30, occupation: 'Software engineer' }, { name: 'Kate Müller', age: 28 }];
+const pet = {
+    swim: () => console.log('swim'),
+};
+const day = 'Monday';
 //# sourceMappingURL=index.js.map
