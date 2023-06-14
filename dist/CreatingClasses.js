e9074e428ef9af3245127bde51dafb86a57e1e0c
Irene Fuente Romero <ifuenteromero@gmail.com>
2023-06-14 19:48:28 +0200

    static members

--- dist/CreatingClasses.js
+++ dist/CreatingClasses.js
@@ -1,23 +1,49 @@
 "use strict";
 class Account {
-    constructor(id, owner, balance) {
+    constructor(id, owner, _balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = _balance;
     }
     deposit(amount) {
         if (amount <= 0)
             throw new Error('Invalid amount');
-        this.balance += amount;
+        this.calculateTax();
+        this._balance += amount;
     }
-    getBalance() {
-        return this.balance;
+    get balance() {
+        return this._balance;
+    }
+    calculateTax() {
+        console.log('hola');
     }
 }
 let account = new Account(1, 'Ireene', 10);
 account.deposit(15000);
 console.log(typeof account);
 console.log(account instanceof Account);
-console.log(account.getBalance());
 console.log({ account });
+console.log(account.balance);
+class SeatAssignment {
+}
+let seats = new SeatAssignment;
+seats.A1 = 'Mosh';
+seats.A2 = 'Irene';
+class Ride {
+    start() {
+        Ride._activeRides++;
+    }
+    stop() {
+        Ride._activeRides--;
+    }
+    static get activeRides() {
+        return Ride._activeRides;
+    }
+}
+Ride._activeRides = 0;
+let ride1 = new Ride;
+ride1.start();
+let ride2 = new Ride;
+ride2.start();
+console.log({ activeRides: Ride.activeRides });
 //# sourceMappingURL=CreatingClasses.js.map
--- src/CreatingClasses.ts
+++ src/CreatingClasses.ts
@@ -59,4 +59,28 @@ seats.A1 = 'Mosh';
 seats.A2 = 'Irene'
 
 
+class Ride {
+    private static _activeRides: number = 0;
+
+    start() {
+        Ride._activeRides++
+    }
+
+    stop() {
+        Ride._activeRides--
+    }
+
+    static get activeRides(): number {
+        return Ride._activeRides
+    }
+}
+
+let ride1 = new Ride;
+ride1.start()
+
+let ride2 = new Ride;
+ride2.start()
+
+console.log({ activeRides: Ride.activeRides });
+
 
