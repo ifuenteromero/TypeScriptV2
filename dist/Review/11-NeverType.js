7d10561cb5cb70f015cbee08ab4c31b52452f039
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-10-09 19:25:02 +0200

    The never type

---
+++ dist/Review/11-NeverType.js
@@ -0,0 +1,9 @@
+"use strict";
+const processEvents = () => {
+    while (true) {
+        console.log('Processing events...');
+    }
+};
+processEvents();
+console.log('This will never be reached!');
+//# sourceMappingURL=11-NeverType.js.map
---
+++ src/Review/11-NeverType.ts
@@ -0,0 +1,19 @@
+// The never type
+// The never type represents values that never occur.
+// For example, a function that always throws an error or
+// never returns a value has a return type of never.
+
+function processEvents(): never {
+	while (true) {
+		console.log('Processing events...');
+	}
+}
+
+function throwError(message: string): never {
+	throw new Error(message);
+}
+// Si las pongo como arrow functions, no sale el warning de unreachable code
+// processEvents();
+throwError('This is an error!');
+
+console.log('This will never be reached!');
