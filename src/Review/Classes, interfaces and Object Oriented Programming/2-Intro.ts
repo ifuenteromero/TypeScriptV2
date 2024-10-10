2dc279277e4e7ffe56eb0c325d06804144d1e827
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-10-10 12:40:04 +0200

    What is OOP

---
+++ src/Review/Classes, interfaces and Object Oriented Programming/2-Intro.ts
@@ -0,0 +1,9 @@
+/*Object oriented programming is one of the many programming paradigms (styles of programmming)
+    in which objects are the building blocks of applications.
+
+    These programming paradigms are styles or ways of writing code. They are not programming languages.
+    Different programming languages support different programming paradigms.
+    
+    For example, JavaScript and TypeScript both support some object oriented and functional programming techniques.
+    
+*/
